ca04583feb6173249c6f3d843f93bea764410f3e
Klaus Strießnig <klaus.striessnig@dynatrace.com>
2024-03-09 10:36:02 +0100

    doc(types): Add JSDoc to decode method (#137)
    Signed-off-by: Klaus Striessnig <k.striessnig@gmail.com>

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -2,6 +2,10 @@ declare module '@discordjs/opus' {
 	export class OpusEncoder {
 		public constructor(rate: number, channels: number);
 		public encode(buf: Buffer): Buffer;
+		/**
+		 * Decodes the given Opus buffer to PCM signed 16-bit little-endian
+		 * @param buf Opus buffer
+		 */
 		public decode(buf: Buffer): Buffer;
 		public applyEncoderCTL(ctl: number, value: number): void;
 		public applyDecoderCTL(ctl: number, value: number): void;
