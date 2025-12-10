4baa4afb7654ebc7b479cce60b290fcc43d9e6c1
almeidx <github@almeidx.dev>
2025-12-10 19:27:59 +0000

    feat(OpusEncoder): make `rate` and `channels` optional

--- tests/test.js
+++ tests/test.js
@@ -3,15 +3,23 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { OpusEncoder } = require('../lib/index.js');
 
-const opus = new OpusEncoder(16_000, 1);
+// Encoding test
+{
+  const opus = new OpusEncoder(16_000, 1);
+  
+  const frame = fs.readFileSync(path.join(__dirname, 'frame.opus'));
+  
+  const decoded = opus.decode(frame);
+  
+  const reEncoded = opus.encode(decoded);
+  
+  assert(decoded.length === 640, 'Decoded frame length is not 640');
+  assert(reEncoded.length === 45, 're encoded frame length is not 45');
+}
 
-const frame = fs.readFileSync(path.join(__dirname, 'frame.opus'));
-
-const decoded = opus.decode(frame);
-
-const reEncoded = opus.encode(decoded);
-
-assert(decoded.length === 640, 'Decoded frame length is not 640');
-assert(reEncoded.length === 45, 're encoded frame length is not 45');
+// Default values work
+{
+  new OpusEncoder();
+}
 
 console.log('Passed');
